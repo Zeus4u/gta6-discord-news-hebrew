efe674906a251c7ec8aa8d161cba22dbbc29446a
Zeus4u <94016042+Zeus4u@users.noreply.github.com>
2026-03-11 16:49:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
-import os
 import requests
+from bs4 import BeautifulSoup
+from deep_translator import GoogleTranslator
+import os
 
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
+url = "https://www.mixvale.com.br/?s=gta+vi"
+
+res = requests.get(url)
+soup = BeautifulSoup(res.text, "html.parser")
+
+article = soup.find("h2")
+
+title = article.text
+link = article.find("a")["href"]
+
+title_he = GoogleTranslator(source='auto', target='iw').translate(title)
+
 message = {
-    "content": "✅ בדיקת דמו: הבוט מחובר בהצלחה ושולח הודעות לדיסקורד"
+    "content": f"🎮 חדשות GTA VI\n\n{title_he}\n\n{link}"
 }
 
 requests.post(WEBHOOK, json=message)
-print("Test message sent")
